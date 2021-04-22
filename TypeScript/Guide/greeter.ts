c00aa9a4adb29e2a79bccb768d08a1d7e2af584a
doraemon <26552699@qqq.com>
2021-04-22 17:40:44 +0800

    add 4.22

---
+++ TypeScript/Guide/greeter.ts
@@ -0,0 +1,41 @@
+// function greeter(person: string){
+//     return 'Hello' + person
+// }
+
+// let user = "chu"
+// // let user = [1,2,3]
+// document.body.innerHTML = greeter(user)
+
+// interface Person {
+//     firstName : string;
+//     lastName : string
+// }
+// function greeter(person: Person){
+//     return "hello" + person.firstName + person.lastName
+// }
+// let user = {firstName:'chu',lastName:'1'}
+// console.log(greeter(user))
+
+// class Student {
+//   fullName: string;
+//   constructor(public firstName, public middleInitial, public lastName) {
+//     this.fullName = firstName + middleInitial + lastName;
+//   }
+// }
+// interface Person {
+//   firstName: string;
+//   lastName: string;
+// }
+
+// function greeter(person: Person) {
+//   return "hello" + person.firstName + person.lastName;
+// }
+
+// let user = new Student("Jane", "M", "User");
+// console.log(greeter(user));
+
+
+
+
+
+
