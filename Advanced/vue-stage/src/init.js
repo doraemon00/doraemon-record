ca99a322fa5f24e8687f8783066aeb4adf9ad3c6
doraemon <26552699@qqq.com>
2021-07-14 09:19:42 +0800

    add vue的实现流程

--- Advanced/vue-stage/src/init.js
+++ Advanced/vue-stage/src/init.js
@@ -13,6 +13,18 @@ export function initMixin(Vue){
         if(vm.$options.el){
         // 要将数据挂载到页面上 
 
+        // 现在数据已经被劫持了 数据变化需要更新视图 diff算法更新需要更新的部分
+        //vue -> template    jsx->(灵活)
+        // vue3 template 写起来性能会更高一些 内部做了很多优化
+
+        // template -> ast语法树 (用来描述语法的，描述语法本身的) -> 描述成一个树结构 -> 将代码重组js语法
+        // 模板编译原理（把template模板编译成render函数 -> 虚拟DOM -> diff算法比对虚拟DOM ）
+
+        // ast -> render返回 —> vnode ->生成真实dom
+        //        更新的时候再次调用render -> 新的 vnode -> 新旧对比 -> 更新真实dom
+
+
+
         }
 
 
