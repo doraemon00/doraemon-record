84387cd994bda9ba947aae5e541ff80ea061dd4c
doraemon00 <26552699@qqq.com>
2021-09-29 17:24:14 +0800

    add 9.29

---
+++ DailyStudy/Sep/9.29/a.js
@@ -0,0 +1,25 @@
+// export default 形式导出的，导入时可以自己定义并且命名
+// let a = 5
+// let b = 6
+// export default {a,b} 
+
+
+// 普通导出
+// let a = 5
+// let b = 6
+// export {a,b} 
+
+
+// 模块化方式导出
+export function a(){
+    console.log("module a ")
+}
+
+// 或者 下面写法都是一样的 
+// function a(){}
+// export {a}
+
+
+
+
+
---
+++ DailyStudy/Sep/9.29/b.js
@@ -0,0 +1,14 @@
+// export default 形式导出的，导入时可以自己定义并且命名
+// import object from './a.js'
+// console.log(object)
+
+
+//  普通导入 {} 形式
+// import {a,b} from './a.js'
+// console.log(a,b)
+
+
+
+// 模块形式导入 
+import * as module1 from './a.js'
+console.log(module1)
